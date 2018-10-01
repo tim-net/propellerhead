80b3a230c1a4d967a75eee19559d9c746c7636ab
tnetisov <tnetisov@amt.ru>
2018-10-01 13:14:55 +0300

    fixed angular test

--- src/main/webapp/src/app/app.component.spec.ts
+++ src/main/webapp/src/app/app.component.spec.ts
@@ -14,15 +14,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'webapp'`, async(() => {
+  it(`should have as title 'Customers application'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('webapp');
+    expect(app.title).toEqual('Customers application');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to webapp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Customers application!');
   }));
 });
