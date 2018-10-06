dee8133f1d3fd24bbc8f85c2a76a57a0ecc87eaa
Timofei Netisov <netisov@gmail.com>
2018-10-06 21:36:50 +0300

    test Jenkins auto deployment

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Customers application';
+  title = 'Customers application, now runnning on AWS EC2 instance';
 }
